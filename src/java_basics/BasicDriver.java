6033c0b50bc8683c7afd80c1c50b084106b02d76
Greg Wagner <gregory.m.wagner@gmail.com>
2017-06-23 09:10:14 -0700

    Added first block of code

---
+++ src/java_basics/BasicDriver.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package java_basics;
+
+/**
+ * @author Greg Wagner- Snoozle Learning
+ * Created June 23, 2017
+ * 
+ * This program will read in two number add them to together
+ * and right them back out
+ */
+import java.util.Scanner;
+
+public class BasicDriver {
+
+	/**
+	 * @param args
+	 * 
+	 * Program driver
+	 */
+	public static void main(String[] args) {
+		// initializing the scanner class
+		Scanner scanner = new Scanner(System.in);
+		
+		// Prompt user to read in value
+		System.out.println("Enter an integer: ");
+		int integer1 = scanner.nextInt();
+		
+		System.out.println("Enter another integer: ");
+		int integer2 = scanner.nextInt();
+		
+		scanner.close();
+		
+		int sumValue = integer1 + integer2;
+		
+		System.out.println("The sum of the values is " + sumValue);
+		
+
+	}
+
+}
