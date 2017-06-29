220af8656371d63bceb3e59c68887fe02d1ddd3f
Greg Wagner <gregory.m.wagner@gmail.com>
2017-06-28 21:53:02 -0700

    Added commits to methods code and rename a package

--- src/main/java/methodsLesson4/Methods.java
+++ src/main/java/methodsLesson4/Methods.java
@@ -1,4 +1,4 @@
-package methodsLesson;
+package methodsLesson4;
 
 /***
  * 
@@ -15,24 +15,35 @@ public class Methods {
 	 */
 	public static void main(String[] args) {
 		
+		// no parameter or return value
 		random1();
 		random1();
 		random1();
 		
+		// initialize variables
 		int min = 1;
 		int max = 10;
 		
+		// parameters but not return value
 		random2(min,max);
 		random2(min,max);
 		
+		// parameters and return values
 		int rand1 = random3(min, max);
 		int rand2 = random3(min, max);
 		
+		// outputs
 		System.out.println("Rand1 = " + rand1);
 		System.out.println("Rand2 = " + rand2);
 
 	}
 	
+	/***
+	 * Description rand with just return values
+	 * @param min
+	 * @param max
+	 * @return
+	 */
 	public static int random3(int min, int max)
 	{
 		Random rand = new Random();
